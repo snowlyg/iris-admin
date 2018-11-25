f5774aa70e48da256957e079fed29dd813e3ed6d
dreamfish <569616226@qq.com>
2018-11-25 14:35:10 +0800

    合并 auth_token.go 和 jwt.go 文件

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,8 +1,12 @@
 package middleware
 
 import (
+	"IrisYouQiKangApi/models"
 	"github.com/dgrijalva/jwt-go"
 	jwtmiddleware "github.com/iris-contrib/middleware/jwt"
+	"github.com/kataras/iris"
+	"net/http"
+	"time"
 )
 
 /**
@@ -18,4 +22,33 @@ func JwtHandler() *jwtmiddleware.Middleware {
 		SigningMethod: jwt.SigningMethodHS256,
 	})
 
+}
+
+/**
+ * 判断 token 是否有效
+ * 如果有效 就获取信息并且保存到请求里面
+ * @method AuthToken
+ * @param  {[type]}  ctx       iris.Context    [description]
+ */
+func AuthToken(ctx iris.Context) {
+
+	u := ctx.Values().Get("jwt").(*jwt.Token)   //获取 token 信息
+	token := models.GetOauthTokenByToken(u.Raw) //获取 access_token 信息
+	if token.Revoked == 1 || token.ExpressIn < time.Now().Unix() {
+		ctx.StatusCode(http.StatusUnauthorized)
+		ctx.JSON(models.ApiJson{Status: false, Data: "", Msg: "token 已经过期"})
+		ctx.Next()
+
+		return
+	}
+
+	user := new(models.Users)
+	user.ID = token.UserId
+
+	user.GetUserById() //获取 user 信息
+
+	ctx.Values().Set("auth_user_id", user.ID)
+	ctx.Values().Set("auth_user_name", user.Name)
+
+	ctx.Next() // execute the "after" handler registered via `DoneGlobal`.
 }

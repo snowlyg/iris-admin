31ad2639b0a1cf12d877a04332bcb39cfa6d6668
luodan@elinkport.com <linximeng>
2020-01-13 14:33:47 +0800

    更新说明文档

--- permissions_test.go
+++ permissions_test.go
@@ -10,6 +10,6 @@ func TestPermissions(t *testing.T) {
 	getMore(t, "/v1/admin/permissions", iris.StatusOK, true, "操作成功", nil)
 }
 
-func TestImportPermissions(t *testing.T) {
-	bImport(t, "/v1/admin/permissions/import", iris.StatusOK, true, "操作成功", nil)
-}
+//func TestImportPermissions(t *testing.T) {
+//	bImport(t, "/v1/admin/permissions/import", iris.StatusOK, true, "操作成功", nil)
+//}

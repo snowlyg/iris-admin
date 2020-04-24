301e8273ddab591cd9ca6f20df4f810a76e72b47
snowlyg <569616226@qq.com>
2020-04-24 09:08:46 +0800

    修改mysql init 文件版本

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,5 +1,1 @@
--- use mysql;
--- ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
 create database custom;
-use custom;
-source /root/go/src/BeeCustom/database/sql/bee_custom.sql;

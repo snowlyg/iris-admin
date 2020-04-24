44c6578d4025c7c99a42091e61e69258cfc946c0
snowlyg <569616226@qq.com>
2020-04-24 08:36:58 +0800

    修改mysql init 文件版本

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,14 +1,5 @@
 use mysql;
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
-create database test;
-use test;
-create table user
-(
-    id int auto_increment primary key,
-    username varchar(64) unique not null,
-    email varchar(120) unique not null,
-    password_hash varchar(128) not null,
-    avatar varchar(128) not null
-);
-insert into user values(1, "mysql","test12345@qq.com","passwd","avaterpath");
-insert into user values(2, "lisi","12345test@qq.com","passwd","avaterpath");
+create database custom;
+CREATE USER 'beecustom'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
+GRANT ALL PRIVILEGES ON *.* TO 'beecustom'@'%';

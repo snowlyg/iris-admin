5a70f055d1853fd30e9a1e7a0eb3349c04fc3266
snwolyg <569616226@qq.com>
2021-12-15 22:39:44 +0800

    增加 pid 打印显示

--- server/web/web_gin/run_darwin.go
+++ server/web/web_gin/run_darwin.go
@@ -1,6 +1,8 @@
 package web_gin
 
 import (
+	"fmt"
+	"syscall"
 	"time"
 
 	"github.com/fvbock/endless"
@@ -9,6 +11,10 @@ import (
 
 func initServer(address string, router *gin.Engine) server {
 	s := endless.NewServer(address, router)
+	s.BeforeBegin = func(add string) {
+		fmt.Printf("Actual pid is %d\n", syscall.Getpid())
+		// save it somehow
+	}
 	s.ReadHeaderTimeout = 10 * time.Millisecond
 	s.WriteTimeout = 10 * time.Second
 	s.MaxHeaderBytes = 1 << 20

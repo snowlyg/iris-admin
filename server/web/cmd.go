1818e803230bf6a5d771055d6c11e00d2caca4a1
snwolyg <569616226@qq.com>
2021-12-27 15:27:41 +0800

    增加配置文件初始化方法

--- server/web/cmd.go
+++ server/web/cmd.go
@@ -1,23 +1,22 @@
-package web_iris
+package web
 
 import (
 	"fmt"
 	"strings"
 
 	"github.com/snowlyg/iris-admin/server/database"
-	"github.com/snowlyg/iris-admin/server/web"
 )
 
 // InitConfig 初始化配置文件
 func InitConfig() error {
 	var cover string
-	if web.IsExist() {
+	if IsExist() {
 		fmt.Println("Your web config is initialized , reinitialized web will cover your web config.")
 		fmt.Println("Did you want to do it ?  [Y/N]")
 		fmt.Scanln(&cover)
 		switch strings.ToUpper(cover) {
 		case "Y":
-			err := web.Remove()
+			err := Remove()
 			if err != nil {
 				return err
 			}
@@ -28,18 +27,18 @@ func InitConfig() error {
 			fmt.Scanln(&dbType)
 			switch dbType {
 			case "1":
-				web.CONFIG.System.DbType = "mysql"
+				CONFIG.System.DbType = "mysql"
 				if err := database.InitConfig(); err != nil {
 					return err
 				}
 			default:
-				web.CONFIG.System.DbType = "mysql"
+				CONFIG.System.DbType = "mysql"
 				if err := database.InitConfig(); err != nil {
 					return err
 				}
 			}
 
-			web.InitWeb()
+			InitWeb()
 		case "N":
 			return nil
 		default:

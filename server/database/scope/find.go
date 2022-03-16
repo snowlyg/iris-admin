57b30a94dafb63e66f74b2d9226dab9641dd2f8d
snwolyg <569616226@qq.com>
2022-03-16 14:31:11 +0800

    更新scop支持

--- server/database/scope/find.go
+++ server/database/scope/find.go
@@ -18,6 +18,22 @@ func InIdsScope(ids []uint) func(db *gorm.DB) *gorm.DB {
 	}
 }
 
+// InNamesScope 根据 name 查询
+// - names 数据
+func InNamesScope(names []string) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Where("name in ?", names)
+	}
+}
+
+// InUuidsScope 根据 uuid 查询
+// - uuid
+func InUuidsScope(uuids []string) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Where("uuid in ?", uuids)
+	}
+}
+
 // NeIdScope 根据 !=id 查询
 // - id id
 func NeIdScope(id uint) func(db *gorm.DB) *gorm.DB {

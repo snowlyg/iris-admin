e10966b23a5dc2b0fef4357fca26c07496726a1b
snowlyg <569616226@qq.com>
2022-06-28 11:14:02 +0800

    add do once job function

--- server/cron_server/index.go
+++ server/cron_server/index.go
@@ -2,7 +2,9 @@ package cron_server
 
 import (
 	"errors"
+	"fmt"
 	"sync"
+	"time"
 
 	"github.com/robfig/cron/v3"
 )
@@ -23,4 +25,18 @@ func CronInstance() *cron.Cron {
 		cc = cron.New(cron.WithSeconds())
 	})
 	return cc
+}
+
+// DoOnce run job once time,this job will run after 2 second
+func DoOnce(job cron.Job, t ...time.Duration) error {
+	once := time.Now().Add(2 * time.Second)
+	if len(t) == 1 {
+		once = time.Now().Add(t[0] * time.Second)
+	}
+	onceSpec := fmt.Sprintf("%d %d %d %d %d %d", once.Second(), once.Minute(), once.Hour(), once.Day(), once.Month(), once.Weekday())
+	_, err := CronInstance().AddJob(onceSpec, job)
+	if err != nil {
+		return err
+	}
+	return nil
 }
